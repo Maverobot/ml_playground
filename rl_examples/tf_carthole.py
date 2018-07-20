c6e41bf090ad890e20520127d79958a09fd98c07
Maverobot <quzhengrobot@gmail.com>
2018-07-20 23:35:27 +0200

    cleaned it up a bit

--- rl_examples/tf_carthole.py
+++ rl_examples/tf_carthole.py
@@ -8,31 +8,21 @@ import matplotlib.pyplot as plt
 # Definition of this game: https://github.com/openai/gym/wiki/CartPole-v0
 # This code is inspired by https://medium.com/emergent-future/simple-reinforcement-learning-with-tensorflow-part-0-q-learning-with-tables-and-neural-networks-d195264329d0
 
-def tf_choose(a, prob):
-    elems = tf.convert_to_tensor(a)
-    samples = tf.multinomial(tf.log(prob), num_samples=1) # note log-prob
-    return elems[tf.cast(samples[0][0], tf.int32)]
-
 env = gym.make('CartPole-v0')
 tf.reset_default_graph()
 
 # NN parameters
 input_n = 4
-hidden_n = 12
 output_n = 2
 
 # Description of TF graph
 state = tf.placeholder(shape=[1,input_n], dtype=tf.float32)
-W1 = tf.Variable(tf.random_uniform([input_n, hidden_n], 0, 0.01), name="W1")
-b1 = tf.Variable(tf.random_normal([1, hidden_n], 0, 0.01), name="b1")
-W2 = tf.Variable(tf.random_uniform([hidden_n, output_n], 0, 0.01), name="W2")
-b2 = tf.Variable(tf.random_normal([1, output_n], 0, 0.01), name="b2")
-H = tf.nn.relu(tf.add(tf.matmul(state, W1), b1))
-Q_values = tf.add(tf.matmul(H, W2), b2)
-
-# TODO: this line causes local minima and the algorithm will simply not converge
-Q_prob = tf.nn.softmax(Q_values)
-action = tf_choose([0,1], Q_prob)
+hidden = tf.layers.dense(state, 128, activation=tf.nn.relu)
+hidden = tf.layers.dropout(hidden, 0.1)
+hidden = tf.layers.dense(hidden, 64, activation=tf.nn.relu)
+hidden = tf.layers.dropout(hidden, 0.1)
+Q_values = tf.layers.dense(hidden, 2, activation=tf.nn.relu)
+action = tf.argmax(Q_values, 1)
 
 next_Q_values = tf.placeholder(shape=[1,output_n], dtype=tf.float32)
 loss = tf.reduce_sum(tf.square(next_Q_values- Q_values))
@@ -41,7 +31,7 @@ opt_op = opt.minimize(loss)
 
 # Learning parameters
 gamma = 0.99
-random_exploration_rate = 0.1
+random_exploration_rate = 0.5
 
 # Training the network
 init = tf.global_variables_initializer()
@@ -49,6 +39,7 @@ num_episodes = 3000
 
 # Create lists to contain total rewards
 rewards = []
+actions = []
 with tf.Session() as sess:
     sess.run(init)
     for i in range(num_episodes):
@@ -65,11 +56,13 @@ with tf.Session() as sess:
 
             # Explore randomly
             if np.random.uniform(0, 1) < random_exploration_rate:
-                a = env.action_space.sample()
+                a[0] = env.action_space.sample()
+
+            actions.append(a[0])
 
             # Run one step simulation and get feedback
-            observation, reward, done, _ = env.step(a)
-            env.render()
+            observation, reward, done, _ = env.step(a[0])
+            # env.render()
             observation = np.reshape(observation, (1, 4))
 
             # Obtain the Q' values by feeding the new state through our network
@@ -78,7 +71,7 @@ with tf.Session() as sess:
             # Obtain maxQ' and set our target value for chosen action.
             maxQ1 = np.max(Q1)
             targetQ = Q
-            targetQ[0, a] = reward + gamma * maxQ1
+            targetQ[0, a[0]] = reward + gamma * maxQ1
 
             # Train our network using target and predicted Q values
             _ = sess.run([opt_op],feed_dict={state:observation,next_Q_values:targetQ})
@@ -86,16 +79,19 @@ with tf.Session() as sess:
             s = observation
             if done:
                 break
-
-        # plot rewards
-        plt.clf()
-        plt.plot(rewards)
-        plt.draw()
-        plt.pause(0.0001)
+        rewards.append(total_rewards)
         if i % 10 == 0:
+            # plot rewards
+            plt.figure(1)
+            plt.clf()
+            plt.plot(rewards)
+            plt.figure(2)
+            plt.clf()
+            plt.hist(actions)
+            plt.draw()
+            plt.pause(0.01)
             print('Iteration #%d -- Total reward = %d.' %
                   (i, total_rewards))
-        rewards.append(total_rewards)
 
 env.close()
 input("Press Enter to continue...")
